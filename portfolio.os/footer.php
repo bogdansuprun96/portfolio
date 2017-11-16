35dc92cb5307ad2a8805bc11511340f7f83c3060
Bohdan <bogdansuprun96@gmail.com>
2017-11-17 01:08:54 +0200

    1. Переробив footer. Замість колонок зробив поки що рядок. 2. Додав блок вибору мови. 3. Оптимізував фонову картинку. 4. Додав та оптимізував блок з номером телефону. 5. Додав соціальні кнопки. 6. Перейменував пункти меню. 7. Оптимізував фото та меню. 8. Зменшив розмір першого блоку. 9. Вивів іконки з прапорами країн за допомогою background. 10. Підключив font-awesome. А також тестовий шрифт.

--- portfolio.os/footer.php
+++ portfolio.os/footer.php
@@ -1,41 +1,53 @@
 <div class="row panel-footer">
-    <div class="col-md-3">
-        <ul>
-            <li>Hello</li>
-            <li>Hello</li>
-            <li>Hello</li>
-            <li>Hello</li>
-            <li>Hello</li>
-        </ul>
-    </div>
-    <div class="col-md-3">
-        <ul>
-            <li>Hello</li>
-            <li>Hello</li>
-            <li>Hello</li>
-            <li>Hello</li>
-            <li>Hello</li>
-        </ul>
-    </div>
-    <div class="col-md-3">
-        <ul>
-            <li>Hello</li>
-            <li>Hello</li>
-            <li>Hello</li>
-            <li>Hello</li>
-            <li>Hello</li>
-        </ul>
-    </div>
-    <div class="col-md-3">
-        <ul>
-            <li>Hello</li>
-            <li>Hello</li>
-            <li>Hello</li>
-            <li>Hello</li>
-            <li>Hello</li>
-        </ul>
+    <!--    <div class="col-md-3">
+            <ul>
+                <li>Hello</li>
+                <li>Hello</li>
+                <li>Hello</li>
+                <li>Hello</li>
+                <li>Hello</li>
+            </ul>
+        </div>
+        <div class="col-md-3">
+            <ul>
+                <li>Hello</li>
+                <li>Hello</li>
+                <li>Hello</li>
+                <li>Hello</li>
+                <li>Hello</li>
+            </ul>
+        </div>
+        <div class="col-md-3">
+            <ul>
+                <li>Hello</li>
+                <li>Hello</li>
+                <li>Hello</li>
+                <li>Hello</li>
+                <li>Hello</li>
+            </ul>
+        </div>
+        <div class="col-md-3">
+            <ul>
+                <li>Hello</li>
+                <li>Hello</li>
+                <li>Hello</li>
+                <li>Hello</li>
+                <li>Hello</li>
+            </ul>
+        </div>-->
+    <div class="row">
+        <div class="col-md-6" id="social-icons">
+            <a href="#"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-vk" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-google-plus-square" aria-hidden="true"></i></a>
+        </div>
+        <div class="col-md-6">
+
+        </div>
     </div>
 </div>
 </div>
+<script src="assets/js/bootstrap.js"></script>
 </body>
 </html>
--- portfolio.os/header.php
+++ portfolio.os/header.php
@@ -11,5 +11,33 @@
 <body>
 <div class="container">
     <div class="row header">
-
+        <h1 class="name">Bohdan Suprun</h1>
+        <img src="images/avatar.jpg" class="img-responsive img-thumbnail" id="avatar-thumbnail" alt="">
+        <div id="lang" class="col-md-12">
+            <ul class="icons-lang-ul">
+                <li><a href="#"><i id="usa" class="icons-lang"></i></a></li>
+                <li><a href="#"><i id="ua" class="icons-lang"></i></a></li>
+                <li><a href="#"><i id="ru" class="icons-lang"></i></a></li>
+            </ul>
+        </div>
+        <div id="callback">
+            <span>Tell-me:</span>
+            <h3><i>(068)</i>578-67-48</h3>
+        </div>
+    </div>
+    <div class="row nav">
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Web-Coders</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Resume</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Price</a></li>
+                    <li><a href="#">FAQ</a></li>
+                </ul>
+            </div>
+        </nav>
     </div>
