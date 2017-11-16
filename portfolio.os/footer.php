9d671c1756fdd5bda4c3afa3a83f2316a5a17d8f
Bohdan <bogdansuprun96@gmail.com>
2017-11-16 22:53:51 +0200

    Додав блок з фоном в header.php. Зробив modal-panel з випадковим списком на 4 блоки. Нормалізував фонову картинку.

---
+++ portfolio.os/footer.php
@@ -0,0 +1,40 @@
+<div class="row panel-footer">
+    <div class="col-md-3">
+        <ul>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+        </ul>
+    </div>
+    <div class="col-md-3">
+        <ul>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+        </ul>
+    </div>
+    <div class="col-md-3">
+        <ul>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+        </ul>
+    </div>
+    <div class="col-md-3">
+        <ul>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
+        </ul>
+    </div>
+</div>
+</body>
+</html>
