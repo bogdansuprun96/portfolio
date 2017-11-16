95aafcd0bbf2ca4469a0f4afcbbb108c383a6179
Bohdan <bogdansuprun96@gmail.com>
2017-11-17 01:13:51 +0200

    Дозалив.

--- portfolio.os/footer.php
+++ portfolio.os/footer.php
@@ -38,9 +38,10 @@
     <div class="row">
         <div class="col-md-6" id="social-icons">
             <a href="#"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
-            <a href="#"><i class="fa fa-vk" aria-hidden="true"></i></a>
             <a href="#"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
             <a href="#"><i class="fa fa-google-plus-square" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-github-square" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-vk" aria-hidden="true"></i></a>
         </div>
         <div class="col-md-6">
 
